d7fca75b6074c80a1694651cc94eaf738f156be0
Jan Langheimer <langheimer@gmail.com>
2019-04-13 20:00:24 +0200

    router scroll and active class behavior added

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,14 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
   routes: [
     {
       path: '/',
